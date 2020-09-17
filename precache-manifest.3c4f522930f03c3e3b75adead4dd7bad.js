0ace0aa161bb97cb22914e42c8bb624c921692c4
alexasteria <replay.faq@gmail.com>
2020-09-17 18:32:21 +0300

    Updates

---
+++ precache-manifest.3c4f522930f03c3e3b75adead4dd7bad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae332068ea41a006b35de1f58e3671b2",
+    "url": "/possum/index.html"
+  },
+  {
+    "revision": "36bc83206c23bd74312d",
+    "url": "/possum/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "03b3bcef7ac0e8f0de2d",
+    "url": "/possum/static/css/main.f90f9a55.chunk.css"
+  },
+  {
+    "revision": "36bc83206c23bd74312d",
+    "url": "/possum/static/js/2.a068acc0.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/possum/static/js/2.a068acc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b3bcef7ac0e8f0de2d",
+    "url": "/possum/static/js/main.1c90821c.chunk.js"
+  },
+  {
+    "revision": "2021fc5356747e15d6de",
+    "url": "/possum/static/js/runtime-main.053b7312.js"
+  }
+]);
