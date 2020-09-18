27a3ae8634812beabcbecce1db13bcc5720f48d2
alexasteria <replay.faq@gmail.com>
2020-09-18 23:27:31 +0300

    Updates

---
+++ precache-manifest.d9ae6031adf5defa2d567bd41de35384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179320be35de9a2f77c43813cc5e6bcd",
+    "url": "/possum/index.html"
+  },
+  {
+    "revision": "ed70908be82a781e8135",
+    "url": "/possum/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "3a09d89ebc6482ee8799",
+    "url": "/possum/static/css/main.604c181c.chunk.css"
+  },
+  {
+    "revision": "ed70908be82a781e8135",
+    "url": "/possum/static/js/2.6498008f.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/possum/static/js/2.6498008f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a09d89ebc6482ee8799",
+    "url": "/possum/static/js/main.c820e96c.chunk.js"
+  },
+  {
+    "revision": "2021fc5356747e15d6de",
+    "url": "/possum/static/js/runtime-main.053b7312.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/possum/precache-manifest.86fa15d60ba5c18fab234e377e07c79f.js"
+  "/possum/precache-manifest.d9ae6031adf5defa2d567bd41de35384.js"
 );
 
 self.addEventListener('message', (event) => {
