f3e42ff7cebe6d086c254fa9f8f2371577b3039b
Alex <alexasteria@icloud.com>
2020-10-22 16:43:34 +0300

    Updates

--- precache-manifest.3781b298d876019dd6b0b379087f2d2e.js
+++ precache-manifest.3781b298d876019dd6b0b379087f2d2e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6199af3c59a3cdfd23aa148201fb852d",
+    "revision": "f89f7a499cc111cb1f6cd07692903b77",
     "url": "/possum/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/possum/static/css/2.09e62520.chunk.css"
   },
   {
-    "revision": "efa3df7f1bf95b34b0ab",
+    "revision": "98647a628fd01419c248",
     "url": "/possum/static/css/main.30b76cfe.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/possum/static/js/2.dd3a8e90.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "efa3df7f1bf95b34b0ab",
-    "url": "/possum/static/js/main.c766f5f3.chunk.js"
+    "revision": "98647a628fd01419c248",
+    "url": "/possum/static/js/main.981adf18.chunk.js"
   },
   {
     "revision": "2021fc5356747e15d6de",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/possum/precache-manifest.430475e19eae0a20d69f44bb75684e15.js"
+  "/possum/precache-manifest.3781b298d876019dd6b0b379087f2d2e.js"
 );
 
 self.addEventListener('message', (event) => {
